b43d26f7a3166f50cc40c1adf81e89a03996ab19
Taisuke Miyazaki <imishinist@gmail.com>
2016-10-14 17:21:30 +0900

    kmeans++を実装

--- k_means.hpp
+++ k_means.hpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 #include <random>
 #include <cmath>
 #include <chrono>
@@ -56,6 +57,51 @@ bool assign_means(std::vector<int>& belongs_to, const std::vector<std::vector<do
 }
 
 template<typename T>
+void initialize_kpp(std::vector<int>& belongs_to, std::vector<std::vector<double>>& means, const std::vector<std::vector<T>>& points, int k) {
+	std::random_device rnd;
+	std::mt19937 mt(rnd());
+	std::uniform_int_distribution<> r(0, belongs_to.size() - 1);
+    int f_i = r(mt);
+    for (int i = 0; i < points[f_i].size(); i++) {
+        means[0][i] = points[f_i][i];
+    }
+
+    int selected = 1;
+
+    while(selected < k) {
+        std::vector<double> d(points.size(), 0);
+        double D = 0.0;
+
+        for (int i = 0; i < points.size(); i++) {
+            double dist = double_inf;
+            double disti = 0;
+            for (int j = 0; j < selected; j++) {
+                double tmp = distance(points[i], means[j]);
+                if (tmp < dist) {
+                    dist = tmp;
+                    disti = j;
+                }
+            }
+            D += dist;
+            d[i] = dist;
+            belongs_to[i] = disti;
+        }
+
+        double sum = 0.0;
+        std::vector<double> tables;
+        for (int i = 0; i < d.size(); i++) {
+            sum += d[i] / D;
+            tables.push_back(sum);
+        }
+        std::uniform_real_distribution<> a(0, 1);
+        auto pos = std::upper_bound(tables.begin(), tables.end(), a(mt));
+        int index = std::distance(tables.begin(), pos);
+        means[selected] = points[index];
+        selected++;
+    }
+}
+
+template<typename T>
 std::vector<int> k_means(const std::vector<std::vector<T>>& points, int k) {
 	std::vector<int> belongs_to(points.size(), 0);
 	std::vector<std::vector<double>> means(k, std::vector<double>(points[0].size(), 0));
@@ -63,11 +109,12 @@ std::vector<int> k_means(const std::vector<std::vector<T>>& points, int k) {
 	std::random_device rnd;
 	std::mt19937 mt(rnd());
 	std::uniform_int_distribution<> r(0, k - 1);
-	for (auto& x : belongs_to) x = r(mt);
+	//for (auto& x : belongs_to) x = r(mt);
+    initialize_kpp(belongs_to, means, points, k);
 
-	do {
+    while (assign_means(belongs_to, means, points)) {
 		calc_means(belongs_to, means, points);
-	} while(assign_means(belongs_to, means, points));
+    }
 
 	return belongs_to;
 }
